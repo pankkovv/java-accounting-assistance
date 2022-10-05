ab529a4aa511273ed71d1b33c32608ff177c3dbe
Панков Денис <Pankkovv@yandex.ru>
2022-10-05 14:23:52 +0300

    Исправлена общая логика работы, добавлена проверка на считывание

--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,50 @@ public class Main {
         MonthlyReport monthlyReport = new MonthlyReport();
         YearlyReport yearlyReport = new YearlyReport();
 
-        System.out.println("Здравствуйте! Выберете команду:");
+        boolean chekingMonth = false;
+        boolean chekingYear = false;
+
+        System.out.println("Здравствуйте! Введите путь до папки, где хранятся месячные и годовые отчеты:");
+        String path =  scanner.next();
+        System.out.println("Выберете команду:");
         printMenu();
         int userInput = scanner.nextInt();
+
         while (userInput != 0){
             int command = userInput;
-
             switch (command) {
-            case (1):
-                  System.out.println(readFile.readFileMonthOrNull());
-                  System.out.println();
-                break;
-            case (2):
-                System.out.println(readFile.readFileYearOrNull());
+            case 1:
+                System.out.println(readFile.readFileMonthOrNull(path));
+                chekingMonth = readFile.chekingExecutionMonth;
                 System.out.println();
                 break;
-            case (3):
-                dataReconciliation.reconciliation();
+            case 2:
+                System.out.println(readFile.readFileYearOrNull(path));
+                chekingYear = readFile.chekingExecutionYear;
+                System.out.println();
                 break;
-            case (4):
-                System.out.println(monthlyReport.processingMonth());
+            case 3:
+                if(chekingMonth && chekingYear) {
+                    System.out.println(dataReconciliation.reconciliation(path));
+                }else{
+                    System.out.println("Сначала необходимо считать месячные и годовые отчеты." + "\n");
+                }
                 break;
-            case (5):
-                System.out.println(yearlyReport.processingYear());
+            case 4:
+                if(chekingMonth) {
+                    System.out.println(monthlyReport.processingMonth(path));
+                }else{
+                    System.out.println("Сначала необходимо считать месячные отчеты." + "\n");
+                }
                 break;
-            case (0000):
+            case 5:
+                if(chekingYear) {
+                    System.out.println(yearlyReport.processingYear(path));
+                }else{
+                    System.out.println("Сначала необходимо считать годовой отчет." + "\n");
+                }
+                break;
+            case 0000:
                 return;
             default:
                 System.out.println("Такого значние не существует, попробуйте еще раз.");
