5fb82edc51169287c3a4a801d1d5a5fc18a43844
Панков Денис <Pankkovv@yandex.ru>
2022-10-05 14:26:56 +0300

    Исправлена общая логика, сделан рефакторинг

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -1,186 +1,91 @@
 import java.util.*;
 
 public class MonthlyReport {
-    HashMap<String, Integer> month = new HashMap<>();
+    HashMap<String, Integer> calculationMonth = new HashMap<>();
     HashMap<String, Integer> reportMonthYan = new HashMap<>();
     HashMap<String, Integer> reportMonthFeb = new HashMap<>();
     HashMap<String, Integer> reportMonthMarch = new HashMap<>();
 
     ReadFile readFile = new ReadFile();
 
-    public String processingMonth() {
-        int c = 0;
-        Integer profitMax;
-        Integer profitMin;
-        String nameMonth = "";
+    public String processingMonth(String path) {
+        int count = 0;
         String resultReportYan = "";
         String resultReportFeb = "";
         String resultReportMarch = "";
-        String categoryProfitMax;
-        String categoryProfitMin;
-
-        readFile.readFileMonthOrNull();
-
-        for (int i = 1; i < 4; i++) {
-            if(i == 1){
-                nameMonth = "Январь";
-            } else if(i == 2){
-                nameMonth = "Февраль";
-            } else if(i == 3){
-                nameMonth = "Март";
-            }
-
-            switch (i) {
-                case (1):
-                    profitMax = 0;
-                    profitMin = 0;
-                    categoryProfitMax = "";
-                    categoryProfitMin = "";
+        String result = "";
 
 
-                    ArrayList<String> yan = readFile.readMonthYan;
+            readFile.readFileMonthOrNull(path);
 
-                    while(c < yan.size()){
-                        Integer sum = 0;
-                        String category = "";
+            for(Integer i : readFile.months.keySet()){
+                String nameMonth = readFile.months.get(i);
 
-                        category = yan.get(c);
+                Integer profitMax = 0;
+                Integer profitMin = 0;
+                String categoryProfitMax = "";
+                String categoryProfitMin = "";
+                ArrayList<String> month = new ArrayList<>();
 
-                        if(yan.get(c+1).equals("TRUE")){
-                            sum = -1 * Integer.parseInt(yan.get(c+2)) * Integer.parseInt(yan.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        } else {
-                            sum = Integer.parseInt(yan.get(c+2))*Integer.parseInt(yan.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        }
+                if(i == 1){
+                    month = readFile.readMonthYan;
+                }else if(i == 2){
+                    month = readFile.readMonthFeb;
+                }else if(i == 3){
+                    month = readFile.readMonthMarch;
+                }
 
-                        c += 4;
+
+                while(count < month.size()){
+                    Integer sum = 0;
+                    String category = "";
+
+                    category = month.get(count);
+
+                    if(month.get(count+1).equals("TRUE")){
+                        sum = -1 * Integer.parseInt(month.get(count+2)) * Integer.parseInt(month.get(count+3));
+                        calculationMonth.put(category, sum);
+                    } else {
+                        sum = Integer.parseInt(month.get(count+2))*Integer.parseInt(month.get(count+3));
+                        calculationMonth.put(category, sum);
                     }
-                    c = 0;
 
-                    for(String category : month.keySet()){
+                    count += 4;
+                }
+                count = 0;
 
-                        int profit = month.get(category);
+                for(String category : calculationMonth.keySet()){
+                    int profit = calculationMonth.get(category);
 
-                        if(profit > profitMax){
-                            profitMax =  profit;
-                            categoryProfitMax = category;
-                        }
-
-                        if(profit < profitMin){
-                            profitMin = profit;
-                            categoryProfitMin = category;
-                        }
-
+                    if(profit > profitMax){
+                        profitMax =  profit;
+                        categoryProfitMax = category;
                     }
+
+                    if(profit < profitMin){
+                        profitMin = profit;
+                        categoryProfitMin = category;
+                    }
+                }
+
+                if(i == 1){
                     reportMonthYan.put(categoryProfitMax, profitMax);
                     reportMonthYan.put(categoryProfitMin, profitMin);
-
-                    month.clear();
                     resultReportYan = nameMonth + "\n" + "Самый прибыльный товар: " + categoryProfitMax + " " + Integer.toString(reportMonthYan.get(categoryProfitMax)) + "\n" + "Самая большая трата: " + categoryProfitMin + " " + Integer.toString(-1 * reportMonthYan.get(categoryProfitMin)) + "\n";
-                    break;
-
-                case(2):
-                    profitMax = 0;
-                    profitMin = 0;
-                    categoryProfitMax = "";
-                    categoryProfitMin = "";
-                    ArrayList<String> feb = readFile.readMonthFeb;
-                    while(c < feb.size()){
-                        int sum = 0;
-                        String category = "";
-
-                        category = feb.get(c);
-
-                        if(feb.get(c+1).equals("TRUE")){
-                            sum = -1 * Integer.parseInt(feb.get(c+2)) * Integer.parseInt(feb.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        } else {
-                            sum = Integer.parseInt(feb.get(c+2))*Integer.parseInt(feb.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        }
-
-                        c += 4;
-                    }
-                    c = 0;
-
-                    for(String category : month.keySet()){
-
-                        int profit = month.get(category);
-
-                        if(profit > profitMax){
-                            profitMax =  profit;
-                            categoryProfitMax = category;
-                        }
-
-                        if(profit < profitMin){
-                            profitMin = profit;
-                            categoryProfitMin = category;
-                        }
-
-                    }
+                }else if(i == 2){
                     reportMonthFeb.put(categoryProfitMax, profitMax);
                     reportMonthFeb.put(categoryProfitMin, profitMin);
-
-                    month.clear();
-                    resultReportFeb = nameMonth + "\n" + "Самый прибыльный товар: " + categoryProfitMax + " " + Integer.toString(reportMonthFeb.get(categoryProfitMax)) + "\n" + "Самая большая трата: " + " " + categoryProfitMin + Integer.toString(-1 * reportMonthFeb.get(categoryProfitMin)) + "\n";
-                    break;
-
-                case(3):
-                    profitMax = 0;
-                    profitMin = 0;
-                    categoryProfitMax = "";
-                    categoryProfitMin = "";
-                    ArrayList<String> march = readFile.readMonthMarch;
-
-                    while(c < march.size()){
-                        int sum = 0;
-                        String category = "";
-
-                        category = march.get(c);
-
-                        if(march.get(c+1).equals("TRUE")){
-                            sum = -1 * Integer.parseInt(march.get(c+2)) * Integer.parseInt(march.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        } else {
-                            sum = Integer.parseInt(march.get(c+2))*Integer.parseInt(march.get(c+3));
-                            month.put(category, sum);
-                            sum = 0;
-                        }
-
-                        c += 4;
-                    }
-                    c = 0;
-
-                    for(String category : month.keySet()){
-
-                        int profit = month.get(category);
-
-                        if(profit > profitMax){
-                            profitMax =  profit;
-                            categoryProfitMax = category;
-                        }
-
-                        if(profit < profitMin){
-                            profitMin = profit;
-                            categoryProfitMin = category;
-                        }
-
-                    }
+                    resultReportFeb = nameMonth + "\n" + "Самый прибыльный товар: " + categoryProfitMax + " " + Integer.toString(reportMonthFeb.get(categoryProfitMax)) + "\n" + "Самая большая трата: " + categoryProfitMin + " " + Integer.toString(-1 * reportMonthFeb.get(categoryProfitMin)) + "\n";
+                }else if(i == 3){
                     reportMonthMarch.put(categoryProfitMax, profitMax);
                     reportMonthMarch.put(categoryProfitMin, profitMin);
-
-                    month.clear();
                     resultReportMarch = nameMonth + "\n" + "Самый прибыльный товар: " + categoryProfitMax + " " + Integer.toString(reportMonthMarch.get(categoryProfitMax)) + "\n" + "Самая большая трата: " + categoryProfitMin + " " + Integer.toString(-1 * reportMonthMarch.get(categoryProfitMin)) + "\n";
-                    break;
+                }
+
+                calculationMonth.clear();
             }
-        }
-        String result = resultReportYan + "\n" + resultReportFeb + "\n" + resultReportMarch;
+            result = resultReportYan + "\n" + resultReportFeb + "\n" + resultReportMarch + "\n";
+
         return result;
     }
 }
--- src/YearlyReport.java
+++ src/YearlyReport.java
@@ -6,103 +6,90 @@ public class YearlyReport {
 
     ReadFile readFile = new ReadFile();
 
-    public String processingYear(){
-        readFile.readFileYearOrNull();
+    public String processingYear(String path){
+
+        readFile.readFileMonthOrNull(path);
+        readFile.readFileYearOrNull(path);
+
+        HashMap<Integer, String> reportMonth = readFile.months;
         ArrayList<String> reportYear = readFile.readYear;
+
         ArrayList<Double> averageExpenseMin = new ArrayList<>();
         ArrayList<Double> averageExpenseMax = new ArrayList<>();
+
         String nameYear = "Год 2021";
-        Double profitMin = 0.0;
-        Double profitMax = 0.0;
+        Double averageMin = 0.0;
+        Double averageMax = 0.0;
 
-        int c = 0;
+        int count = 0;
 
-        while(c < reportYear.size()) {
-            Double profitCount = 0.0;
+        while(count < reportYear.size()) {
+            Double profitMonth = 0.0;
             String category = "";
 
-            if (reportYear.get(c).equals("01")) {
+
+
+            if (reportYear.get(count).split(",")[0].equals("01")) {
                 category = "Январь";
-            } else if (reportYear.get(c).equals("02")) {
+            } else if (reportYear.get(count).split(",")[0].equals("02")) {
                 category = "Февраль";
-            } else if (reportYear.get(c).equals("03")) {
+            } else if (reportYear.get(count).split(",")[0].equals("03")) {
                 category = "Март";
             }
 
-            ArrayList<Double> count = new ArrayList<>();
-            Double profitMonth = 0.0;
-            int m = 0;
-            while(m < 6) {
-                if (reportYear.get(c + m + 2).equals("false")) {
-                    profitMonth = Double.parseDouble(reportYear.get(c + m + 1));
-                    count.add(profitMonth);
-                } else if (reportYear.get(c + m + 2).equals("true")) {
-                    profitMonth = -1 * Double.parseDouble(reportYear.get(c+ m + 1));
-                    count.add(profitMonth);
+            int meter = 0;
+            while(meter < reportYear.size()/reportMonth.size()) {
+                if (reportYear.get(count).split(",")[2].equals("false")) {
+                    profitMonth += Double.parseDouble(reportYear.get(count).split(",")[1]);
+                } else if (reportYear.get(count).split(",")[2].equals("true")) {
+                    profitMonth += -1 * Double.parseDouble(reportYear.get(count).split(",")[1]);
                 }
-                m += 3;
+                meter += 1;
+                count += 1;
             }
+                count -= meter;
+            year.put(category, profitMonth);
 
-
-            for(Double profit : count){
-                profitCount += profit;
-                year.put(category, profitCount);
-            }
-
-            count.clear();
-
-            Double average = 0.0;
-            m = 0;
-            while(m < 6) {
-                if (reportYear.get(c + m + 2).equals("true")) {
-                    average = -1 * Double.parseDouble(reportYear.get(c+ m + 1));
-                    averageExpenseMin.add(average);
+            meter = 0;
+            while(meter < reportYear.size()/reportMonth.size()) {
+                if (reportYear.get(count).split(",")[2].equals("true")) {
+                    averageMin += -1 * Double.parseDouble(reportYear.get(count).split(",")[1]);
+                } else if (reportYear.get(count).split(",")[2].equals("false")){
+                    averageMax += Double.parseDouble(reportYear.get(count).split(",")[1]);
                 }
-                m += 3;
+                meter += 1;
+                count += 1;
             }
+            count -= meter;
+            count += 2;
 
-            average = 0.0;
-            m = 0;
-            while(m < 6) {
-                if (reportYear.get(c + m + 2).equals("false")) {
-                    average = Double.parseDouble(reportYear.get(c+ m + 1));
-                    averageExpenseMax.add(average);
-                }
-                m += 3;
-            }
-
-            c += 6;
         }
+        year.put("Средний расход за все месяцы в году", (-1 * averageMin)/reportMonth.size());
+        year.put("Средний доход за все месяцы в году", averageMax/reportMonth.size());
+        reportYear.clear();
 
-        for(Double profit : averageExpenseMin){
-            profitMin += profit;
-        }
-        year.put("Средний расход за все месяцы в году", (-1 * profitMin)/3);
-
-        for(Double profit : averageExpenseMax){
-            profitMax += profit;
-        }
-        year.put("Средний доход за все месяцы в году", profitMax/3);
-
-
-        String category = "";
         String resultYan = "";
         String resultFeb = "";
         String resultMarch = "";
-        for (int i = 0; i < 3; i++)
-        if (i == 0) {
-            category = "Январь";
-            resultYan = category + ", прибыль составила: " + Double.toString(year.get(category));
-        } else if (i == 1) {
-            category = "Февраль";
-            resultFeb = category + ", прибыль составила: " + Double.toString(year.get(category));
-        } else if (i == 2) {
-            category = "Март";
-            resultMarch = category + ", прибыль составила: " + Double.toString(year.get(category));
+        String resultExpense = "";
+        String resultIncome = "";
+
+        for (String category : year.keySet()){
+            if (category.equals("Январь")) {
+                resultYan = category + ", прибыль составила: " + Double.toString(year.get(category));
+            } else if (category.equals("Февраль")) {
+                resultFeb = category + ", прибыль составила: " + Double.toString(year.get(category));
+            } else if (category.equals("Март")) {
+                resultMarch = category + ", прибыль составила: " + Double.toString(year.get(category));
+            }
         }
 
+        resultExpense = "Средний расход за все месяцы в году: " + Double.toString(year.get("Средний расход за все месяцы в году"));
 
-        String result = nameYear  + "\n" + resultYan  + "\n" + resultFeb  + "\n" + resultMarch  + "\n" + "Средний расход за все месяцы в году: " + Double.toString(year.get("Средний расход за все месяцы в году")) + "\n" +"Средний доход за все месяцы в году: " + Double.toString(year.get("Средний доход за все месяцы в году")) + "\n";
+        resultIncome = "Средний доход за все месяцы в году: " + Double.toString(year.get("Средний доход за все месяцы в году"));
+
+
+        String result = nameYear  + "\n" + resultYan  + "\n" + resultFeb  + "\n" + resultMarch  + "\n" + resultExpense + "\n" + resultIncome + "\n";
         return result;
     }
 }
