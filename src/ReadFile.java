aaaa300849f6d9ba4e3fff9825c462a4571bf1b8
Панков Денис <Pankkovv@yandex.ru>
2022-10-05 14:24:48 +0300

    Исправлено чтение файлов, теперь все идет по общей логике без привязки к месяцу + проверка на наличие файлов

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,86 +1,93 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ReadFile {
+
+    HashMap<Integer, String> months = new HashMap<>();
     ArrayList<String> readMonthYan = new ArrayList<>();
     ArrayList<String> readMonthFeb = new ArrayList<>();
     ArrayList<String> readMonthMarch = new ArrayList<>();
     ArrayList<String> readYear = new ArrayList<>();
 
-    public String readFileMonthOrNull() {
+    boolean chekingExecutionMonth;
+    boolean chekingExecutionYear;
+
+    public String readFileMonthOrNull(String path) {
+
+        for(int i = 1; i < 4; i++){
+            switch (i) {
+                case 1:
+                    months.put(i, "Январь");
+                    break;
+                case 2:
+                    months.put(i, "Февраль");
+                    break;
+                case 3:
+                    months.put(i, "Март");
+                    break;
+            }
+        }
 
         try {
-            for(int i = 1; i < 4; i++) {
-                List<String> list;
-                switch (i){
-                    case(1):
-                        list = (ArrayList<String>) Files.readAllLines(Path.of("resources\\m.20210" + i + ".csv"));
-                        for(int j = 1; j < list.size(); j++){
-                            String[] lines = list.get(j).split(System.lineSeparator());
-                            for(String line : lines){
-                                String[] lineContents = line.split(",");
-                                for(String contents : lineContents){
+            String check = "";
+            for(Integer i : months.keySet()) {
+                if(Files.exists(Path.of(path + File.separator + "m.20210" + i + ".csv"))){
+                    List<String> list;
+                    list = Files.readAllLines(Path.of(path + File.separator + "m.20210" + i + ".csv"));
+                    for (int j = 1; j < list.size(); j++) {
+                        String[] lines = list.get(j).split(System.lineSeparator());
+                        for (String line : lines) {
+                            String[] lineContents = line.split(",");
+                            for (String contents : lineContents) {
+                                if(i == 1){
                                     readMonthYan.add(contents);
-                                }
-                            }
-                        }
-                        list.clear();
-                        break;
-                    case(2):
-                        list = (ArrayList<String>) Files.readAllLines(Path.of("resources\\m.20210" + i + ".csv"));
-                        for(int j = 1; j < list.size(); j++){
-                            String[] lines = list.get(j).split(System.lineSeparator());
-                            for(String line : lines){
-                                String[] lineContents = line.split(",");
-                                for(String contents : lineContents){
+                                }else if(i == 2){
                                     readMonthFeb.add(contents);
-                                }
-                            }
-                        }
-                        list.clear();
-                        break;
-                    case(3):
-                        list = (ArrayList<String>) Files.readAllLines(Path.of("resources\\m.20210" + i + ".csv"));
-                        for(int j = 1; j < list.size(); j++){
-                            String[] lines = list.get(j).split(System.lineSeparator());
-                            for(String line : lines){
-                                String[] lineContents = line.split(",");
-                                for(String contents : lineContents){
+                                } else if(i == 3){
                                     readMonthMarch.add(contents);
                                 }
                             }
                         }
-                        list.clear();
-                        break;
                     }
+                    list.clear();
+                    check =  "Считывание месячных отчетов прошло успешно.";
+                    chekingExecutionMonth = true;
+                } else {
+                    check = "Файл не находится в нужной директории.";
+                    chekingExecutionMonth = false;
                 }
-            return "Считывание месячных отчетов прошло успешно.";
+            }
+            return check;
         } catch (IOException e) {
-            System.out.println("Невозможно прочитать файл с месячным отчётом. Возможно, файл не находится в нужной директории.");
-            return null;
+            return "Невозможно прочитать файл с месячным отчётом.";
         }
     }
 
-    public String readFileYearOrNull()
-    {
+    public String readFileYearOrNull(String path) {
         try {
-        List<String> list = (ArrayList<String>) Files.readAllLines(Path.of("resources\\y.2021.csv"));
-        for(int i = 1; i < list.size(); i++){
-            String[] lines = list.get(i).split(System.lineSeparator());
-            for(String line : lines){
-                String[] lineContents = line.split(",");
-                for(String contents : lineContents){
-                    readYear.add(contents);
+            String check = "";
+            if(Files.exists(Path.of(path + File.separator + "y.2021.csv"))){
+                List<String> list = Files.readAllLines(Path.of(path + File.separator + "y.2021.csv"));
+                for(int i = 1; i < list.size(); i++){
+                    String[] lines = list.get(i).split(System.lineSeparator());
+                    for (String line : lines){
+                        readYear.add(line);
+                    }
                 }
+                check = "Считывание годового отчета прошло успешно.";
+                chekingExecutionYear = true;
+            }else {
+                check = "Файл не находится в нужной директории.";
             }
-        }
-        return "Считывание годового отчета прошло успешно.";
+        return check;
     } catch (IOException e) {
-        System.out.println("Невозможно прочитать файл с месячным отчётом. Возможно, файл не находится в нужной директории.");
-        return null;
+        return "Невозможно прочитать файл с годовым отчётом.";
     }
     }
+
 }
